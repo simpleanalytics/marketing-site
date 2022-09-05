1ea52f760cf1471da8297f2acee3f31503ba16ea
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2022-09-05 14:02:15 +0200

    Fix docs link on mobile

--- components/MenuLink.vue
+++ components/MenuLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a @click="href && router.push({ path: href })" :target="target">
+  <a @click="clickHandler">
     <slot />
   </a>
 </template>
@@ -8,7 +8,13 @@
 // We can not use <NuxtLink> with headlessui:
 // - https://headlessui.com/react/menu#integrating-with-next-js
 
-defineProps(["href", "target"]);
+const props = defineProps(["href", "target"]);
 
 const router = useRouter();
+
+const clickHandler = () => {
+  if (props.href?.startsWith("/")) router.push({ path: props.href });
+  else if (props.target) window.open(props.href, props.target);
+  else window.location.href = props.href;
+};
 </script>
