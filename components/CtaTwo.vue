509caaa409a3437180ea48eed714b6f212ceef81
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2023-02-02 16:06:42 +0700

    Fix Michalin logo in dark mode

--- components/CtaTwo.vue
+++ components/CtaTwo.vue
@@ -3,7 +3,7 @@
     class="bg-gray-100 dark:bg-gray-600 p-4 rounded-lg flex flex-col items-center justify-center"
   >
     <div class="flex items-center justify-center">
-      <Michelin class="w-12 h-12 dark:fill-gray-200" />
+      <Michelin class="w-12 h-12 dark:!fill-gray-200" />
       <span class="mx-6 text-gray-800 dark:text-gray-200"
         >Michelin choose Simple Analytics</span
       >
