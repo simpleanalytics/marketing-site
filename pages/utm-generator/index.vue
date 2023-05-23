c7705c923968babfbb814f7875d6566ba89a4f78
eduek <eduekakpannyah@gmail.com>
2023-05-23 16:53:54 +0100

    fix: overview backgorund colors

--- pages/utm-generator/index.vue
+++ pages/utm-generator/index.vue
@@ -10,15 +10,15 @@
       <div class="justify-center block sm:flex sm:flex-wrap">
         <div v-for="generator in utmGeneratorChannels" class="flex p-6 w-full sm:w-1/2 lg:w-1/3">
           <div class="flex w-full flex-col shadow-xl overflow-hidden rounded-lg bg-blue-100 dark:bg-gray-700 relative">
-            <NuxtLink :to="`/utm-generator/${generator.name.split(' ').join('-')}`" class="flex h-80 p-4 shadow-md" :class="`bg-[${generator.bg}]`">
+            <NuxtLink :to="`/utm-generator/${generator.name}`" :class="`flex h-80 p-4 shadow-md bg-${generator.name}`">
               <div class="flex h-full justify-center items-center w-full">
                 <component :is="generator.imageComponent" class="max-h-full max-w-full"/>
               </div>
             </NuxtLink>
 
             <div class="mt-auto px-6 py-8">
-              <NuxtLink :to="`/utm-generator/${generator.name.split(' ').join('-')}`">
-                <h3 class="capitalize font-semibold text-center dark:text-gray-300 text-gray-600 text-2xl">{{ generator.name }}</h3>
+              <NuxtLink :to="`/utm-generator/${generator.name}`">
+                <h3 class="capitalize font-semibold text-center dark:text-gray-300 text-gray-600 text-2xl">{{ generator.name.split("-").join(" ") }}</h3>
               </NuxtLink>
             </div>
           </div>
@@ -40,27 +40,22 @@ import Tiktok from '@/components/logos/Tiktok.vue';
 const utmGeneratorChannels = ref([
   {
     name: 'facebook',
-    bg: '#add8e6',
     imageComponent: Facebook
   },
   {
     name: 'google',
-    bg: '#add8e6',
     imageComponent: Google
   },
   {
-    name: 'google sheets',
-    bg: '#e6f5ea',
+    name: 'google-sheets',
     imageComponent: GoogleSheets
   },
   {
     name: 'spreadsheets',
-    bg: '#add8e6',
     imageComponent: Excel
   },
   {
     name: 'tiktok',
-    bg: '#add8e6',
     imageComponent: Tiktok
   }
 ]);
@@ -72,3 +67,25 @@ definePageMeta({
   description: "Simple Analytics UTM generator",
 });
 </script>
+
+<style>
+.bg-facebook {
+  @apply bg-[#add8e6]
+}
+
+.bg-google {
+  @apply bg-[#f4b300ec]
+}
+
+.bg-google-sheets {
+  @apply bg-[#e6f5ea]
+}
+
+.bg-spreadsheets {
+  @apply bg-[#90ee90]
+}
+
+.bg-tiktok {
+  @apply bg-[#aaaaaa]
+}
+</style>
