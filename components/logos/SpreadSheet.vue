44696caa4941f4ade960f0639da43d456a0fc331
Atiksh Srivastava <ayushs125@gmail.com>
2023-06-24 02:48:50 +0530

    add - logos and overiew page for utm builders

---
+++ components/logos/SpreadSheet.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    style="
+      shape-rendering: geometricPrecision;
+      text-rendering: geometricPrecision;
+      image-rendering: optimizeQuality;
+      fill-rule: evenodd;
+      clip-rule: evenodd;
+    "
+  >
+    <path
+      style="opacity: 0.999"
+      fill="#6ae2a1"
+      d="M12.5 4.5v8h-8c-1-6.333 1.667-9 8-8Z"
+    />
+    <path
+      style="opacity: 1"
+      fill="#314460"
+      d="M14.5 4.5v8h28c1.333.667 1.333 1.333 0 2h-28v28c-.667 1.333-1.333 1.333-2 0v-28h-8c-1.333-.667-1.333-1.333 0-2h8v-8c.667-1.333 1.333-1.333 2 0Z"
+    />
+    <path
+      style="opacity: 0.992"
+      fill="#6ae2a1"
+      d="M14.5 4.5c8.673-.166 17.34 0 26 .5 1.779 2.135 2.445 4.635 2 7.5h-28v-8Zm-10 10h8v28c-2.865.445-5.365-.221-7.5-2a338.44 338.44 0 0 1-.5-26Z"
+    />
+    <path
+      style="opacity: 0.999"
+      fill="#6ae2a1"
+      d="M42.5 14.5c.166 8.673 0 17.34-.5 26L40.5 42c-8.66.5-17.327.666-26 .5v-28h28Z"
+    />
+  </svg>
+</template>
---
+++ pages/utm-builder/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="max-w-3xl px-4 mx-auto">
+    <div class="text-center mx-4">
+      <h1 class="text-4xl font-medium sm:text-5xl md:text-6xl">
+        {{ $t("utm_builder.overview") }}
+      </h1>
+      <p class="mt-8 text-lg">
+        {{ $t("utm_builder.overview_description") }}
+      </p>
+    </div>
+
+    <div class="mt-10 mb-20 grid gap-4 grid-cols-1 md:grid-cols-2">
+      <NuxtLink
+        v-for="action in actions"
+        :key="action.title"
+        :to="
+          localePath({
+            name: action.section,
+          })
+        "
+        class="group bg-white dark:bg-gray-700 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-red-500 dark:focus-within:ring-red-600 flex flex-col rounded-lg shadow dark:shadow-none"
+      >
+        <div :class="action.iconBackground" class="rounded-lg w-12 inline-flex">
+          <span class="inline-flex p-3">
+            <GoogleLogo v-if="action.type === 'google'" class="h-6 w-6" />
+            <GoogleSheetLogo
+              v-else-if="action.type === 'google-spreadsheet'"
+              class="h-6 w-6"
+            />
+
+            <TwitterLogo
+              v-else-if="action.type === 'twitter'"
+              class="h-6 w-6"
+            />
+
+            <FacebookLogo
+              v-else-if="action.type === 'facebook'"
+              class="h-6 w-6"
+            />
+
+            <TikTokLogo v-else-if="action.type === 'tiktok'" class="h-6 w-6" />
+
+            <SpreadSheetLogo v-else class="h-6 w-6" />
+          </span>
+        </div>
+        <div class="mt-4">
+          <h3 class="text-lg font-medium text-link">
+            {{ $t(action.titleTranslation) }} <Arrow />
+          </h3>
+          <p class="mt-2 text-sm text-gray-500">
+            {{ $t(action.descriptionTranslation) }}
+          </p>
+        </div>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import GoogleLogo from "@/components/logos/Google.vue";
+import FacebookLogo from "@/components/logos/Facebook.vue";
+import GoogleSheetLogo from "@/components/logos/GoogleSheet.vue";
+import SpreadSheetLogo from "@/components/logos/SpreadSheet.vue";
+import TikTokLogo from "@/components/logos/TikTop.vue";
+import TwitterLogo from "@/components/logos/Twitter.vue";
+import Arrow from "@/components/Arrow.vue";
+import { types } from "@/data/utmBuilders.js";
+
+definePageMeta({
+  title: "UTM builder",
+  description: "Simple Analytics UTM builder",
+});
+
+const localePath = useLocalePath();
+
+const actions = types;
+</script>
