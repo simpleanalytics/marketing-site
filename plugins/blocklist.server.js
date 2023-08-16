5ec63c06a5825e77ff68358d011309ba67943d24
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2023-08-16 20:19:11 +0200

    Remove tagparrot blocking because we have ratelimiter

--- plugins/blocklist.server.js
+++ plugins/blocklist.server.js
@@ -1,13 +1,11 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const { event } = nuxtApp.ssrContext;
-  const userAgent = event.node?.req?.headers?.["user-agent"];
-
-  const isBlocked = userAgent?.match(/(tagparrot)/i);
-
-  if (isBlocked)
-    throw createError({
-      statusCode: 403,
-      statusMessage:
-        "You are blocked because you're hitting our servers way to hard.",
-    });
+  // const { event } = nuxtApp.ssrContext;
+  // const userAgent = event.node?.req?.headers?.["user-agent"];
+  // const isBlocked = userAgent?.match(/(tagparrot)/i);
+  // if (isBlocked)
+  //   throw createError({
+  //     statusCode: 403,
+  //     statusMessage:
+  //       "You are blocked because you're hitting our servers way to hard.",
+  //   });
 });
