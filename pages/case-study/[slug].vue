beb3a3ab6c78b8821493061a214c2a6a14cef975
Atiksh <atiksh@Olvys-MacBook-Pro.local>
2023-08-31 11:39:37 +0530

    add: changes for case study urls

---
+++ pages/case-study/[slug].vue
@@ -0,0 +1,15 @@
+<template>
+    <Article
+      name="case-study-slug"
+      :slug="route.params.slug"
+      :articleType="route.params.section"
+      :drafts="true"
+    />
+  </template>
+  
+  <script setup>
+  import Article from "@/components/Article.vue";
+  
+  const route = useRoute();
+  </script>
+  
