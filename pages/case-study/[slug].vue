e3b4bc866c7ed2902ed62c9764ec04f294c3334c
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2023-09-14 09:48:27 +0000

    Run prettier via GitHub Action

--- pages/case-study/[slug].vue
+++ pages/case-study/[slug].vue
@@ -1,14 +1,13 @@
 <template>
-    <Article
-      name="case-study-slug"
-      :slug="route.params.slug"
-      :articleType="route.params.section"
-    />
-  </template>
-  
-  <script setup>
-  import Article from "@/components/Article.vue";
-  
-  const route = useRoute();
-  </script>
-  
+  <Article
+    name="case-study-slug"
+    :slug="route.params.slug"
+    :articleType="route.params.section"
+  />
+</template>
+
+<script setup>
+import Article from "@/components/Article.vue";
+
+const route = useRoute();
+</script>
