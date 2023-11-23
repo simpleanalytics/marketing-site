170ef1d31cf7ccb2b9aa4d64b7f393115ec74e56
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2023-11-23 17:50:48 +0800

    Fix most regexes

--- middleware/redirects.global.js
+++ middleware/redirects.global.js
@@ -1,41 +1,45 @@
 const redirects = [
   {
     type: "regex",
-    from: /^\/en\/glossary\/?$/i,
+    from: /^\/en\/glossary(\/|$)$/i,
     to: "/en/glossaries",
   },
   {
     type: "regex",
-    from: /^\/glossary\/?$/i,
+    from: /^\/glossary(\/|$)/i,
     to: "/glossaries",
   },
   {
     type: "regex",
-    from: /^\/(case-?study|use-cases|use-case|usecase)\/?$/i,
+    from: /^\/(case-?study|use-?cases|use-?case)(\/|$)/i,
     to: "/case-studies",
   },
-  { type: "regex", from: /^\/case-study\/?/i, to: "/case-studies" },
-  { type: "regex", from: /^\/de\/fallstudie\/?/i, to: "/de/fallstudien" },
+  { type: "regex", from: /^\/case-study(\/|$)/i, to: "/case-studies" },
+  { type: "regex", from: /^\/de\/fallstudie(\/|$)/i, to: "/de/fallstudien" },
   {
     type: "regex",
-    from: /^\/es\/estudio-de-caso\//i,
+    from: /^\/es\/estudio-de-caso(\/|$)/i,
     to: "/es/estudios-de-caso",
   },
-  { type: "regex", from: /^\/fr\/etude-de-cas\/?/i, to: "/fr/etudes-de-cas" },
   {
     type: "regex",
-    from: /^\/it\/studio-di-caso\/?/i,
+    from: /^\/fr\/etude-de-cas(\/|$)/i,
+    to: "/fr/etudes-de-cas",
+  },
+  {
+    type: "regex",
+    from: /^\/it\/studio-di-caso(\/|$)/i,
     to: "/it/studi-di-caso",
   },
-  { type: "regex", from: /^\/nl\/casestudie\/?/i, to: "/nl/casestudies" },
+  { type: "regex", from: /^\/nl\/casestudie(\/|$)/i, to: "/nl/casestudies" },
   {
     type: "regex",
-    from: /^\/nl\/glossary\/?$/i,
+    from: /^\/nl\/glossary(\/|$)/i,
     to: "/nl/begrippenlijst",
   },
   {
     type: "regex",
-    from: /^\/glossary\/analytics\/key-term\/key-term-1\/?$/i,
+    from: /^\/glossary\/analytics\/key-term\/key-term-1(\/|$)/i,
     to: "/glossary/analytics/key-term/conversion-funnel",
   },
   {
