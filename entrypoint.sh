1efc04107a2a9dd61198506160c58a3b45141964
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2024-05-07 18:39:10 +0200

    Delete public dir before adding files to it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Copy the pre-existing files to the mounted volume
+rm -rf /mnt/marketing_site_public/*
 cp -r /src/.output/public/* /mnt/marketing_site_public/
 
 # Execute the original entrypoint command
